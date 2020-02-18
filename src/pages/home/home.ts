df5db9eb9beb0a5e4fc7412d5668f88301854168
DouglasEleuterio <douglas-negocios@hotmail.com>
2020-02-17 21:22:51 -0300

    Tela de Login e Página de Categoria
    Criado o Metodo de Login Mackado por enquanto.
    Criada a página de Categoria com navegação após login.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,4 +12,14 @@ export class HomePage {
 
   }
 
+  /**
+   * Ao utilziar o metodo Push.
+   * È realizado o empilhamento de páginas, controlado pelo Ionic.
+   * Inclusive é criado a seta para voltar para a página anterior e a respectiva navegação de back automaticamente.
+   * Ao utilizar setRoot não é construido de forma que automatiza o back e não exibe a seta.
+   */
+  login(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
 }
