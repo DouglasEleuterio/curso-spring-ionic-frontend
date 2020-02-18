305749c17c4e8bd3dd8a3bbd452c1680c5a25efc
DouglasEleuterio <douglas-negocios@hotmail.com>
2020-02-17 21:31:43 -0300

    Desabilitar menu na tela inicial.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {IonicPage, NavController} from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, MenuController, NavController} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,8 +8,17 @@ import {IonicPage, NavController} from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
+  }
 
+  //Quando a página for carregada (ionViewWillEnter) é desabilitado o menu lateral.
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  //Quando a página for Ativada (ionViewDidLeaver) é ativado o menu lateral.
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
   }
 
   /**
@@ -18,7 +27,7 @@ export class HomePage {
    * Inclusive é criado a seta para voltar para a página anterior e a respectiva navegação de back automaticamente.
    * Ao utilizar setRoot não é construido de forma que automatiza o back e não exibe a seta.
    */
-  login(){
+  login() {
     this.navCtrl.setRoot('CategoriasPage');
   }
 
